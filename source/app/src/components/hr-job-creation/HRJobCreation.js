c297c9d8ffa552e69bedbd717a4f425fadf438aa
Chandrakanth2203 <chandrakanth.y2203@gmail.com>
2023-11-29 04:54:52 +0530

    HR JOb Creation

--- source/app/src/components/hr-job-creation/HRJobCreation.js
+++ source/app/src/components/hr-job-creation/HRJobCreation.js
@@ -1,6 +1,7 @@
 import "./HRJobCreation.scss";
 import { Component } from "react";
 import { NavLink } from "react-router-dom";
+import { JobPostingService } from "../../services/job-posting-service";
 
 export default class HRJobCreation extends Component {
     constructor(props) {
@@ -12,63 +13,99 @@ export default class HRJobCreation extends Component {
         };
       }
 
+      postJob = (type) => {
+        const method =
+          JobPostingService.jobPosting
+    
+        method({
+          title: this.state.title,
+          joblocation: this.state.joblocation,
+          lastdate:this.state.lastdate,
+          wages: this.state.wages,
+          jobdescription:this.state.jobdescription,
+          jobqualification:this.state.jobqualification
+        }).then((response) => {
+          if (response.ok) {
+            document.getElementById("post-job-nav-button").click();
+          } else {
+            this.setState(
+              {
+                showErrorMessage: true
+              },
+              () => {
+                setTimeout(() => {
+                  this.setState({
+                    showErrorMessage: false
+                  });
+                }, 5000);
+              }
+            );
+          }
+        });
+      };  
+
     render() {
         return (
-            <div >
-                <div id="home-role-buttons-container">
-                    <label id='HR_Name'>Welcome, {this.props.hrDetails.name}</label>
+            <div>
+                <div id="header-container">
+                    <label name='hr-name'>Welcome, {this.props.hrDetails.name}</label>
                     <NavLink to={"/hr-view-jobs"}>
-                        <div className="hr-page-button">View Posted Jobs</div>
+                        <span id = "hr-nav-button" className="hr-nav-bar-container">View Posted Jobs</span>
                     </NavLink>
                     <NavLink to={"/hr-job-create"}>
-                        <div className="hr-page-button">Post New Job</div>
+                        <span id = "hr-nav-button" className="hr-nav-bar-container">Post New Job</span>
                     </NavLink>
-                    <hr/>
                 </div>
+                <hr/>
                 <div>
-                    <span>
-                        <label class='desc_titles'>Title: </label>
+                    <div className="job-desc-field-container">
+                        <label className='desc_titles'>Title : </label>
                         <input 
                         editable
                         autoComplete="on"
+                        className = "job-posting-input"
                         name="title"
                         type="text"
                         maxLength={40}
                         // value={value}
                         ></input>
-                    </span>
-                    <span>
-                        <label class='desc_titles'>Location: </label>
-                        <select>
+                    </div>
+                    <div className="job-desc-field-container">
+                        <label className='desc_titles'>Location : </label>
+                        <select className = "job-posting-input">
                             <option>Toronto</option>
                             <option>Vancouver</option>
                             <option>Montreal</option>
                         </select>
-                    </span>
-                    <span>
-                        <label class='desc_titles'>Last Date To Apply: </label>
+                    </div>
+                    <div className="job-desc-field-container">
+                        <label className='desc_titles'>Last Date To Apply : </label>
                         <input 
                         editable
+                        className = "job-posting-input"
                         name="lastDate"
                         type="date"
                         maxLength={40}
                         // value={value}
                         ></input>
-                    </span>
-                    <span>
-                        <label class='desc_titles'>Compensation: </label>
+                    </div>
+                    <div className="job-desc-field-container">
+                        <label className='desc_titles'>Compensation : </label>
                         <input
                         editable
+                        className = "job-posting-input"
                         name="lastDate"
-                        type="date"
-                        maxLength={40}
+                        type="number"
+                        placeholder = "$"
+                        maxLength={8}
                         // value={value}
                         ></input>
-                    </span>
-                    <span>
-                        <label class='desc_titles'>Job Description: </label>
+                    </div>
+                    <div className="job-desc-field-container">
+                        <label className='desc_titles'>Job Description : </label>
                         <textarea 
                         editable
+                        className = "job-posting-multi-input"
                         name="jobDescription"
                         type="text"
                         multiline
@@ -76,11 +113,12 @@ export default class HRJobCreation extends Component {
                         maxLength={400}
                         // value={value}
                         ></textarea>
-                    </span>
-                    <span>
-                        <label class='desc_titles'>Qualifications: </label>
+                    </div>
+                    <div className="job-desc-field-container">
+                        <label className='desc_titles'>Job Qualifications : </label>
                         <textarea 
                         editable
+                        className = "job-posting-multi-input"
                         name="jobQualification"
                         type="text"
                         multiline
@@ -88,8 +126,15 @@ export default class HRJobCreation extends Component {
                         maxLength={400}
                         // value={value}
                         ></textarea>
-                    </span>
-                    <button id='submit'> Post </button>
+                    </div>
+                    <div id="job-post-button-container">
+                        <button 
+                        onClick={() => this.postJob()}
+                        className="job-post-button" 
+                        > 
+                        Post 
+                        </button>
+                    </div>
                 </div>
             </div>    
         )
